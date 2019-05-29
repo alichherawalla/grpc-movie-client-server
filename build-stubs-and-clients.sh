c8762a1f9adca714106ce7639e268f3aa9eda96f
Mohammed Ali Chherawalla <mohammed.ali.chherawalla@gmail.com>
2019-05-30 01:15:33 +0800

    using build-project.sh in build-stubs-and-clients.sh

--- build-stubs-and-clients.sh
+++ build-stubs-and-clients.sh
@@ -1,7 +1,5 @@
 cd java-client-and-server
-./gradlew installDist
-./gradlew build
-protoc  --proto_path=src --java_out=build/generated/source/proto/main/java src/main/proto/movie.proto
+bash build-project.sh
 python -m grpc_tools.protoc -I=src/main/proto/ --python_out=clients/python-client/ --grpc_python_out=clients/python-client/ src/main/proto/movie.proto
 grpc_tools_node_protoc -I=src/main/proto/ --js_out=import_style=commonjs,binary:clients/node-client/ --grpc_out=clients/node-client/ --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` src/main/proto/movie.proto
 cd ../
