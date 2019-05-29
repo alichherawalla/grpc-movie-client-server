134ea1a623cc4f2ba917e2eac41d4ca3e33f84f4
Mohammed Ali Chherawalla <mohammed.ali.chherawalla@gmail.com>
2019-05-30 01:23:55 +0800

    cleanup scripts

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 
 osascript -e 'tell app "Terminal"
-    do script "cd /Users/mac/Desktop/grpc/test/gRPCBase/java-client-and-server && bash run-server.sh"
+    do script "cd /Users/mac/Desktop/grpc/test/grpc-movie-client-server/java-client-and-server && bash run-server.sh"
 end tell'
 sleep 1;
 osascript -e 'tell app "Terminal"
-    do script "cd /Users/mac/Desktop/grpc/test/gRPCBase/java-client-and-server && bash run-client.sh"
+    do script "cd /Users/mac/Desktop/grpc/test/grpc-movie-client-server/java-client-and-server && bash run-client.sh"
 end tell'
 sleep 1;
 osascript -e 'tell app "Terminal"
-    do script "cd /Users/mac/Desktop/grpc/test/gRPCBase/node-client && bash run-client.sh"
+    do script "cd /Users/mac/Desktop/grpc/test/grpc-movie-client-server/node-client && bash run-client.sh"
 end tell'
 sleep 1;
 osascript -e 'tell app "Terminal"
-    do script "cd /Users/mac/Desktop/grpc/test/gRPCBase/python-client && bash run-client.sh"
+    do script "cd /Users/mac/Desktop/grpc/test/grpc-movie-client-server/python-client && bash run-client.sh"
 end tell'
 sleep 1;
